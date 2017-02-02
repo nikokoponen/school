fb05f89aa8c2efb484ae1b5d4f62a9091edd9eae
Koponen Niko <a1401031@myy.haaga-helia.fi>
2017-02-02 10:39:36 +0200

    First commit

---
+++ src/main/java/fi/niko/bookstore/web/BookController.java
@@ -0,0 +1,14 @@
+package fi.niko.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class BookController {
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	
+	
+}
